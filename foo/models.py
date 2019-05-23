857cedae77e605f91d298a47986db16dfb1e24e6
Foxugly <rvilain@skynet.be>
2019-05-23 15:56:08 +0200

    Update models.py

--- foo/models.py
+++ foo/models.py
@@ -1,26 +1,11 @@
+from tools.generic_class import GenericClass
 from django.db import models
-from django.urls import reverse
 from django.utils.translation import gettext as _
 
 
 # Create your models here.
-class Bar(models.Model):
+class Bar(GenericClass):
     name = models.CharField(max_length=100, verbose_name=_("name"))
-
-    def get_absolute_url(self):
-        return reverse('foo:bar_change', kwargs={'pk': self.pk})
-
-    def get_add_url(self):
-        return reverse('foo:bar_add')
-
-    def get_detail_url(self):
-        return reverse('foo:bar_detail', kwargs={'pk': self.pk})
-
-    def get_delete_url(self):
-        return reverse('foo:bar_delete', kwargs={'pk': self.pk})
-
-    def get_list_url(self):
-        return reverse('foo:bar_list')
 
     def __str__(self):
         return self.name
@@ -29,23 +14,8 @@ class Bar(models.Model):
         verbose_name = _('Bar')
 
 
-class Multibar(models.Model):
+class Multibar(GenericClass):
     name = models.CharField(max_length=100, verbose_name=_("name"))
-
-    def get_absolute_url(self):
-        return reverse('foo:multibar_change', kwargs={'pk': self.pk})
-
-    def get_add_url(self):
-        return reverse('foo:multibar_add')
-
-    def get_detail_url(self):
-        return reverse('foo:multibar_detail', kwargs={'pk': self.pk})
-
-    def get_delete_url(self):
-        return reverse('foo:multibar_delete', kwargs={'pk': self.pk})
-
-    def get_list_url(self):
-        return reverse('foo:multibar_list')
 
     def __str__(self):
         return self.name
@@ -54,25 +24,10 @@ class Multibar(models.Model):
         verbose_name = _('Multibar')
 
 
-class Foo(models.Model):
+class Foo(GenericClass):
     name = models.CharField(max_length=100, verbose_name=_("name"))
     bar = models.ForeignKey(Bar, on_delete=models.CASCADE, verbose_name=_("bar"))
     multibars = models.ManyToManyField(Multibar, blank=True, verbose_name=_("multibars"))
-
-    def get_absolute_url(self):
-        return reverse('foo:foo_change', kwargs={'pk': self.pk})
-
-    def get_add_url(self):
-        return reverse('foo:foo_add')
-
-    def get_detail_url(self):
-        return reverse('foo:foo_detail', kwargs={'pk': self.pk})
-
-    def get_delete_url(self):
-        return reverse('foo:foo_delete', kwargs={'pk': self.pk})
-
-    def get_list_url(self):
-        return reverse('foo:foo_list')
 
     def __str__(self):
         return self.name
